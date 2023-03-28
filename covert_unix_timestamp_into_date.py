61ebbaf2531e28bea684246809063eb5908c53b6
sivag464 <sivag464>
2023-03-28 12:13:09 +0000

    i have added small date time convertion code

---
+++ covert_unix_timestamp_into_date.py
@@ -0,0 +1,7 @@
+# Databricks notebook source
+from pyspark.sql.functions import col, from_unixtime
+
+df = spark.sparkContext.parallelize([["1590183026"], ["2000000000"]]).toDF(
+    ["ts_col"]
+)
+df = df.withColumn("date_col", from_unixtime(col("ts_col")))
